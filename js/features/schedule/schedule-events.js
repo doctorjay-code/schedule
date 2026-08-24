8930af5600cdc5326fb4803af3c46aaeef7a3822
JAY <skybluesunset95@gmail.com>
2026-08-24 14:07:26 +0900

    feat: 위아래 파란 굵은선 100% 동반 이동 및 내부 칸 구분선 완벽 선명화 배포 (v=20260824_14)

--- js/features/schedule/schedule-events.js
+++ js/features/schedule/schedule-events.js
@@ -20,7 +20,7 @@ let colorLoadPromise = null;
 
 function loadLedgerFeature() {
   if (!ledgerLoadPromise) {
-    ledgerLoadPromise = import('../ledger/index.js?v=20260824_13')
+    ledgerLoadPromise = import('../ledger/index.js?v=20260824_14')
       .then(module => {
         ledgerLifecycle = module.initLedgerView();
         return ledgerLifecycle;
