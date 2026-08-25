bc997ea25002679d6eb8bcb19e4f40836fe35f01
JAY <skybluesunset95@gmail.com>
2026-08-25 23:43:32 +0900

    fix(schedule): restore 2px dark-blue date divider border for schedule weekly table

--- js/version.js
+++ js/version.js
@@ -1,5 +1,5 @@
 // Dynamic Application Version Hub (Auto Cache-Busting Engine)
-export const APP_BUILD_TIME = '20260825_2342';
+export const APP_BUILD_TIME = '20260825_2343';
 
 export function getVersionedUrl(relativePath) {
   const clean = relativePath.split('?')[0];
