bd8397dbc1efb2f2880b3b3140ed6a1eaf8c6dcc
JAY <skybluesunset95@gmail.com>
2026-08-31 11:43:24 +0900

    fix(modal): 모달 3단계 z-index 계층(통계1000->모아보기1100->상세수정1200) 확립으로 모달 뒤에 열리는 버그 완벽 해결 (20260831_1143)

--- js/version.js
+++ js/version.js
@@ -1,5 +1,5 @@
 // Dynamic Application Version Hub (Auto Cache-Busting Engine)
-export const APP_BUILD_TIME = '20260831_1130';
+export const APP_BUILD_TIME = '20260831_1143';
 
 export function getVersionedUrl(relativePath) {
   const clean = relativePath.split('?')[0];
